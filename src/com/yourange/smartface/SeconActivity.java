af789081593b92da1864effcae7db243300eb9c8
YouRange <youcefbenamara@MacBook-Air-de-Youcef.local>
2014-11-14 10:32:33 +0100

    Adding the second activity to my branch

---
+++ src/com/yourange/smartface/SeconActivity.java
@@ -0,0 +1,16 @@
+package com.yourange.smartface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
