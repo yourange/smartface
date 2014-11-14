acdd6110e3b576488d35a8fe89e2e355eecb0dad
YouRange <youcefbenamara@MacBook-Air-de-Youcef.local>
2014-11-14 10:42:57 +0100

    adding a fourth Activity

---
+++ FourthActivity.java
@@ -0,0 +1,17 @@
+
+package com.yourange.smartface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FourthActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+97.247.122.170    albert.apple.com
